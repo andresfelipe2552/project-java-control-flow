3b09765c8316a42c06ab13f89a48b28794fab69e
Andres <andrespr196@gmail.com>
2025-10-31 18:40:08 -0500

    mi primer commit

---
+++ WeatherAdvisor.java
@@ -0,0 +1,9 @@
+public class WeatherAdvisor {
+    public static void main(String[] args) {
+        int temperature = 28; // Temperatura en grados Celsius
+
+        System.out.println("The temperature is " + temperature + "°C.");
+
+        // TODO: Agrega sentencias if-else aquí para dar consejos climáticos
+    }
+}
